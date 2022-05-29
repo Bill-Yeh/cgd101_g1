8f223e17a368835946e1534c90fc741f91fea522
triangleshen <wl00515922@gmail.com>
2022-05-30 00:20:50 +0800

    增加測驗燈箱_三角形

--- src/js/test.js
+++ src/js/test.js
@@ -1,12 +1,23 @@
 
 
 function init(){
-    var test_screenWidth = document.body.clientWidth;
-    let test_50_pic = document.getElementById('test_50_pic');
+    // *-----------開啟燈箱---------* //
+    let test_50 = document.querySelectorAll('.test_50');
 
-    if(test_screenWidth >= 768){
-        test_50_pic.src= 'images/test_topic1.png';
+    for(let i = 0; i < test_50.length; i++){
+        test_50[i].addEventListener('click',function(){
+            testing.style.display = 'block';
+        })
     }
+
+    // *-----------關閉燈箱---------* //
+    let test_lightBox_close = document.querySelector('.test_lightBox_close');
+
+    let testing = document.querySelector('.test_lightBox_bg');
+
+    test_lightBox_close.addEventListener('click',function(){
+        testing.style.display = 'none';
+    })
 }
 
 window.addEventListener('load',init,false);
