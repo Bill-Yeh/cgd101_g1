e82f32263159a4769e33796f9d1f61aeb64a833e
ANJUNI-Sarah <anjuni1996@gmail.com>
2022-06-17 16:08:17 +0800

    更新換裝區

--- src/front_login.php
+++ src/front_login.php
@@ -5,7 +5,7 @@ session_start();
 try{
   $dbname = "tibamefe_cgd101g1";
 	$user = "root";
-	$password = "";
+	$password = "Sarah34302521";
 
 	$dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charse=utf8";
 	$options = [PDO::ATTR_CASE=>PDO::CASE_NATURAL, PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
@@ -29,8 +29,9 @@ try{
     $_SESSION["password"] = $memRow["password"];
     $_SESSION["member_name"] = $memRow["member_name"];
     $_SESSION["coin"] = $memRow["coin"];
+    $_SESSION["role"] = $memRow["role"];
 
-    $result = ["member_id"=>$_SESSION["member_id"], "account"=>$_SESSION["account"], "password"=>$_SESSION["password"], "member_name"=>$_SESSION["member_name"]];
+    $result = ["member_id"=>$_SESSION["member_id"], "account"=>$_SESSION["account"], "password"=>$_SESSION["password"], "member_name"=>$_SESSION["member_name"],"coin"=>$_SESSION["coin"],"role"=>$_SESSION["role"]];
 
     //送出登入者的姓名資料
     echo json_encode($result);
--- src/mem_orderList.php
+++ src/mem_orderList.php
@@ -15,18 +15,50 @@ try{
 
 
     //=========
+
+	$total=[];
+    
 	$sql = "SELECT * FROM `tibamefe_cgd101g1`.`item_record` join `tibamefe_cgd101g1`.`item` 
     on `tibamefe_cgd101g1`.`item_record`.`item_id`=`tibamefe_cgd101g1`.`item`.`item_id` WHERE `member_id` =:mem_id"; 
-    
-    
+
+
 	$products = $pdo->prepare($sql);
 	$products->bindValue("mem_id",$_SESSION["member_id"]);
 	$products->execute();
 
-	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
-	echo json_encode($prodRows);
+	$I_prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
+
+
+	//===========================================================
+
+	$sql = "SELECT * FROM `tibamefe_cgd101g1`.`lesson_record` join `tibamefe_cgd101g1`.`lesson` 
+    on `tibamefe_cgd101g1`.`lesson_record`.`lesson_id`=`tibamefe_cgd101g1`.`lesson`.`lesson_id` WHERE `member_id` =:mem_id"; 
+
+	$lesson = $pdo->prepare($sql);
+	$lesson->bindValue("mem_id",$_SESSION["member_id"]);
+	$lesson->execute();
+
+	$L_prodRows = $lesson->fetchAll(PDO::FETCH_ASSOC);
+
+	for($i=0;$i<count($L_prodRows);$i++){
+		array_push($I_prodRows,$L_prodRows[$i]);
+	}
+
+	// $total=[$I_prodRows,$L_prodRows];
+	// $total.push($L_prodRows);
+
+
+	echo json_encode($I_prodRows);
+
+
+
+
+
+
 }catch(PDOException $e){
+	
 	echo "錯誤訊息 : ", $e->getMessage(), "<br>";
 	echo "錯誤行號 : ", $e->getLine(), "<br>";
+	
 }
 ?>
