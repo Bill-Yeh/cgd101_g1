5a90b97ed2b6f4c1c52f86fbdb0961c18c1f9da9
Wayne14580 <waynew14580@gmail.com>
2022-06-22 11:58:13 +0800

    串接API及資料庫

---
+++ src/N5_word_lesson_record_time.php
@@ -0,0 +1,42 @@
+<?php
+$Date = date('Y-m-d H:i:s', time());
+session_start();
+try{
+    // $dbname = "tibamefe_cgd101g1";
+	// $user = "root"; //帳號
+	// $password = "Apple0810orange"; //密碼
+
+	// $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charse=utf8";
+	// $options = [PDO::ATTR_CASE=>PDO::CASE_NATURAL, PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
+
+	// $pdo = new PDO($dsn, $user, $password, $options);
+    
+    //2.上線之後用的
+    require_once("connect_cgd101g1.php");
+
+	$sql = "select distinct lesson_id from lesson_record where member_id =:member_id and lesson_id = 7 ";
+  	$get_qdata = $pdo->prepare($sql);
+  	$get_qdata->bindValue(":member_id", $_SESSION["member_id"]);
+
+  	$get_qdata->execute();
+
+  	$checkResult = $get_qdata -> rowCount();
+
+ 	if($checkResult == 1){
+    
+    exit();
+  	}else{
+	$sql = "INSERT INTO `lesson_record` (  `member_id`, `lesson_id`,`payment_time`) VALUES (:member_id, :lesson_id, :date_now);"  ;
+	$get_qdata = $pdo->prepare($sql);
+	$get_qdata->bindValue(":member_id", $_SESSION["member_id"]);
+	$get_qdata->bindValue(":lesson_id", $_GET["lesson_id"]);
+	$get_qdata->bindValue(":date_now", $Date);
+	$get_qdata->execute();
+
+	$q_data = $get_qdata->fetchAll(PDO::FETCH_ASSOC);
+
+	}
+}catch(PDOException $e){
+	echo "錯誤訊息 : ", $e->getMessage(), "<br>";
+	echo "錯誤行號 : ", $e->getLine(), "<br>";
+}
