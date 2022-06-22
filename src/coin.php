d159b0ffb01347e6ecb57c866ee0e2df27fe0030
54Yunyun <fsrs90115@gmail.com>
2022-06-22 11:54:29 +0800

    修修改改

--- src/coin.php
+++ src/coin.php
@@ -2,13 +2,13 @@
 session_start();
 try{
 	//=====連接資料庫=====//
-    $dbname = "tibamefe_cgd101g1";
-	$user = "root"; //帳號
-	// $password = "fsrs90115"; //密碼
-	$password = "Sarah34302521"
-	$dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charse=utf8";
-	$options = [PDO::ATTR_CASE=>PDO::CASE_NATURAL, PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
-	$pdo = new PDO($dsn, $user, $password, $options);
+    // $dbname = "tibamefe_cgd101g1";
+	// $user = "root"; 
+	// $password = "Sarah34302521"
+	// $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charse=utf8";
+	// $options = [PDO::ATTR_CASE=>PDO::CASE_NATURAL, PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
+	// $pdo = new PDO($dsn, $user, $password, $options);
+	 require_once("connect_cgd101g1.php");
 if(isset($_SESSION["coin"])){
  echo $_SESSION["coin"];
 
