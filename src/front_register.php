a42617c78e04f6f37cefac4d2b49f58a186523b0
Bill-Yeh <lawyer112240@gmail.com>
2022-06-22 20:54:05 +0800

    修改front_register.php檔，但我不小心在dev開發了

--- src/front_register.php
+++ src/front_register.php
@@ -1,5 +1,5 @@
 <?php
-// session_start();
+session_start();
 // $_POST = json_decode($data_info, true);
 try{
   // $dbname = "tibamefe_cgd101g1";
@@ -23,6 +23,8 @@ try{
     echo json_encode('帳號已存在');
     exit();
   }
+
+  else{
   $sql = "INSERT INTO `member` (`member_id`, `member_name`, `account`, `password`, `member_status`, `coin`, `level`, `role`) VALUES (NULL, :member_name, :account, :password, '1', '0', '1', './images/char_00_0.png')";
 
   $member = $pdo->prepare($sql);
@@ -31,7 +33,37 @@ try{
   $member->bindValue(":password", $_POST["password"]);
 
   $member->execute();
-  echo json_encode('ok');
+
+
+  $sql2 = "select * from `member` where account=:account and password=:password"; 
+  $member2 = $pdo->prepare($sql2);
+  $member2->bindValue(":account", $_POST["account"]);
+  $member2->bindValue(":password", $_POST["password"]);
+  $member2->execute();
+
+  // if( $member->rowCount()==0){ //查無此人
+	//   // echo "exist";
+  //   echo json_encode("exist");
+  // }else{ //登入成功
+  //   //自資料庫中取回資料
+    $memRow = $member2->fetch(PDO::FETCH_ASSOC);
+    //寫入session
+    $_SESSION["member_id"] = $memRow["member_id"];
+    $_SESSION["account"] = $memRow["account"];
+    $_SESSION["password"] = $memRow["password"];
+    $_SESSION["member_name"] = $memRow["member_name"];
+    $_SESSION["coin"] = $memRow["coin"];
+    $_SESSION["role"] = $memRow["role"];
+
+    $result = ["member_id"=>$_SESSION["member_id"], "account"=>$_SESSION["account"], "password"=>$_SESSION["password"], "member_name"=>$_SESSION["member_name"],"coin"=>$_SESSION["coin"],"role"=>$_SESSION["role"]];
+
+    //送出登入者的姓名資料
+    echo json_encode($result);
+
+  }
+
+
+  // echo json_encode('ok');
     // if( $member->rowCount()==0){ //查無此人
     // 	  echo "exist";
     // }else{ //登入成功
